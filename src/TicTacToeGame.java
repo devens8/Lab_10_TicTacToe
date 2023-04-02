e5ce559baea1debb7a56ad2af644d3c4e5cb1b9a
devenseth <devenseth8@gmail.com>
2023-04-02 15:11:41 -0400

    Tie program working - complete

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -13,21 +13,25 @@ public class TicTacToeGame {
     private static String board [][] = new String[ROW][COL];
 
     public static void main(String[] args) {
+        //declare main method variables
         boolean currentMove = true;
         boolean end = false;
         boolean validInput;
         Scanner input =  new Scanner(System.in);//sets up the scanner class
-        System.out.print("\nWelcome to the Tic Tac Toe Game! ");
+        System.out.print("\nWelcome to the Tic Tac Toe Game! "); //Welcome message to begin the game
 
-
+        //do while loop for the y/n question
         do {
             System.out.println("One Player will place X's and the other O's. To begin the game, place the first X by entering the row and column for the spot below \n");
-            clearBoard();
-            display();
+            clearBoard(); //clears the board every time game starts
+            display(); //displays board
+            //do while loop for changing players - X to O
             do {
                 end = false;
+                //do while loop runs until user enters a valid input
                 do {
                     int rowMove = SafeInput.getRangedInt(input, "Please enter the row of your move: ", 1, 3);
+                    //This section changes input to values based off column and row structure
                     if (rowMove == 1) {
                         rowMove = 0;
                     } else if (rowMove == 3) {
@@ -35,12 +39,14 @@ public class TicTacToeGame {
                     }
 
                     int colMove = SafeInput.getRangedInt(input, "Please enter the column of your move: ", 1, 3);
+                    //This section changes input to values based off column and row structure
                     if (colMove == 1) {
                         colMove = 0;
                     } else if (colMove == 3) {
                         colMove = 4;
                     }
 
+                    //This section checks whether the move was valid, and if it is, the move is entered onto the board
                     if (isValidMove(rowMove, colMove)) {
                         validInput = true;
                         if (currentMove) {
@@ -58,6 +64,7 @@ public class TicTacToeGame {
                 System.out.println("");
                 display();
 
+                //This section checks whether the player has won, is a tie, or if the next player should play a move
                 if (isWin("X")) {
                     System.out.println("\nPlayer X has won!");
                     end = true;
@@ -78,7 +85,7 @@ public class TicTacToeGame {
                 }
             } while (!end);
 
-        }while(SafeInput.getYNConfirm(input,"Would you like to play again? Enter Y or N"));
+        }while(SafeInput.getYNConfirm(input,"Would you like to play again? Enter Y or N")); //restarts game if true
     }
 
     // sets all the board elements to a space
@@ -125,6 +132,7 @@ public class TicTacToeGame {
 
     // checks to see if there is a win state on the current board for the specified player (X or O)
     private static boolean isWin(String player){
+        //if one of the win types is true, the player has won
         if(isRowWin(player) || isColWin(player) || isDiagonalWin(player)){
             return true;
         }
@@ -195,7 +203,7 @@ public class TicTacToeGame {
     // checks for a tie condition: all spaces on the board are filled OR there is an X and an O in every win vector
     private static boolean isTie() {
         int counter = 0;
-        //checks each column
+        //checks each column for X
         for (int i = 0; i < ROW; i += 2) {
             for (int j = 0; j < COL; j += 2) {
                 if (board[j][i] == "X") {
@@ -205,7 +213,7 @@ public class TicTacToeGame {
             }
         }
 
-        //checks each row
+        //checks each row for X
         for(int i = 0; i < ROW; i+=2){
             for(int j = 0; j < COL; j+=2){
                 if(board[i][j] == "X"){
@@ -215,6 +223,7 @@ public class TicTacToeGame {
             }
         }
 
+        //checks the first diagonal for X
         int column1 = 0;
         for(int i = 0; i < ROW; i+=2){
             if (board[i][column1] == "X") {
@@ -224,6 +233,7 @@ public class TicTacToeGame {
             column1+=2;
         }
 
+        //checks the second diagonal for X
         int column2 = 4;
         for(int i = ROW-1; i > 0; i-=2){
             if (board[i][column2] == "X") {
@@ -233,7 +243,7 @@ public class TicTacToeGame {
             column2-=2;
         }
 
-        //checks each column
+        //checks each column for O
         for (int i = 0; i < ROW; i += 2) {
             for (int j = 0; j < COL; j += 2) {
                 if (board[j][i] == "O") {
@@ -243,7 +253,7 @@ public class TicTacToeGame {
             }
         }
 
-        //checks each row
+        //checks each row for O
         for(int i = 0; i < ROW; i+=2){
             for(int j = 0; j < COL; j+=2){
                 if(board[i][j] == "O"){
@@ -253,6 +263,7 @@ public class TicTacToeGame {
             }
         }
 
+        //checks the first diagonal for O
         int column3 = 0;
         for(int i = 0; i < ROW; i+=2){
             if (board[i][column3] == "O") {
@@ -262,6 +273,7 @@ public class TicTacToeGame {
             column3+=2;
         }
 
+        //Checks the second diagonal for O
         int column4 = 0;
         for(int i = ROW-1; i >= 0; i-=2){
             if (board[i][column4] == "O") {
@@ -271,6 +283,7 @@ public class TicTacToeGame {
             column4+=2;
         }
 
+        //If the counter is 16 then that means that X is in each row(3), col(3) and diagonal(2), and same for 0 -> 16 is total value
         if(counter == 16){
             return true;
         }
